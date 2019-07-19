047642f61820af24b9d634dab6547b7c4a6d68a0
M <52587340+monzii@users.noreply.github.com>
2019-07-18 22:41:09 -0400

    Update dispatch.sh

--- dispatch.sh
+++ dispatch.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 # Turn Socks Proxy On/off
 nohup bash -c "dispatch start -p 6060 > /dev/null 2>&1" 
+##nohup bash -c "dispatch start -p 6060 --debug > /tmp/dispatchlog.log 2>&1"
 ##OR USE THIS FOR NO OUTPUT## nohup bash -c "dispatch start -p 6060"
